643f66d82192b5250ec2e9e3e06f26d2c623dbf6
Suraj hinge <surajhinge07@gmail.com>
2025-01-20 19:42:24 +0530

    Removed the obsolete App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,30 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./components/Login/Login";
+import Signup from "./components/SignUp/SignUp";
+import HomeDashboard from "./components/HomeDashboard/HomeDashboard";
+import Logout from "./components/Logout/Logout";
+import FlatsDashboard from "./components/Flats/FlatsDashboard";
+import PostProperty from "./components/post-property/post-property";
+import PlotsDashboard from "./components/PlotsDashboard/PlotsDashboard";
+import PostPlot from "./components/Post-plots/Post-plots";
 
 function App() {
   return (
-    <div style={{ textAlign: "center", marginTop: "20px" }}>
-      <h1>Hello!</h1>
-    </div>
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/home" element={<HomeDashboard />} />
+          <Route path="/logout" element={<Logout />} />
+          <Route path="/flats" element={<FlatsDashboard />} />
+          <Route path="/plots" element={<PlotsDashboard />} />
+          <Route path="/post-property" element={<PostProperty />} />
+          <Route path="/post-plot" element={<PostPlot />} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
