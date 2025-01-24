7d427209791d5221af7bf12306197a2d4cca4c2f
Suraj Hinge <surajhinge07@email.com>
2025-01-24 16:28:00 +0530

    Added DNS name

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-s7v!yvgp*&^62_g=jn*el3!mzw%p^5n#9r!6$um$53-gieh*(7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['54.145.26.234', 'localhost']
+ALLOWED_HOSTS = ['ip-172-31-30-3.ec2.internal' ,'54.145.26.234', 'localhost']
 
 
 # Application definition
