e9e1199e08c8f86e66c619cfab105947981572be
Suraj Hinge <surajhinge07@email.com>
2025-01-26 12:02:50 +0530

    Allowded the ip address

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-s7v!yvgp*&^62_g=jn*el3!mzw%p^5n#9r!6$um$53-gieh*(7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['ip-172-31-30-3.ec2.internal' ,'http://107.21.129.33', 'localhost', 'vastyam.com', 'www.vastyam.com']
+ALLOWED_HOSTS = ['ip-172-31-93-191.ec2.internal', '54.210.136.137']
 
 
 # Application definition
@@ -139,7 +139,6 @@ AUTH_USER_MODEL = 'users.CustomUser'
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
-    "https://vastyam.com",
 ]
 
 REST_FRAMEWORK = {
