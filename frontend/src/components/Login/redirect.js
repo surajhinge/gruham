a66ee89a360009f78ec1f339ad252b84beca9e23
Suraj Hinge <surajhinge07@email.com>
2025-01-31 19:11:15 +0530

    modify with Hardcode URL

--- frontend/src/components/Login/redirect.js
+++ frontend/src/components/Login/redirect.js
@@ -1,5 +1,5 @@
 // redirect.js
-if (window.location.protocol === "https:") {
+if (window.location.protocol === "https://backend.vastyam.com:8000") {
     const httpUrl = "http://backend.vastyam.com:8000" + window.location.pathname + window.location.search;
     window.location.replace(httpUrl);
   }
